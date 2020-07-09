989eb4c476b238e56fc2b5d82464a3b10018bac9
Eslam Jekso <jeksogsa@gmail.com>
2020-07-09 06:51:21 +0200

    add Eman as contributor

---
+++ 01-Intro to Computer Science/code/code-with-comment.py
@@ -0,0 +1,16 @@
+# This is a comment
+print('Hello world')
+
+
+# second comment
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world') # inline comment
+
+
+"""
+Test
+Test
+"""
